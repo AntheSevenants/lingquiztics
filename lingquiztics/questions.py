b57d5a7e7cef8664ae85195507a31f411ed8d082
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-10-10 23:23:57 +0200

    Add further revision round support

--- lingquiztics/questions.py
+++ lingquiztics/questions.py
@@ -10,12 +10,15 @@ def load(path):
 def make_text(question):
     return question["description"] + " " + question["question"]
 
-def output_question(question, index):
+def output_question(question, index, revision_round=False, mc_bold=False):
     qmd_content = ""
 
     if not "question" in question:
         raise Exception("question attribute not found in question")
-        
+    
+    if revision_round and "answer" not in question:
+        raise Exception("answer attribute not found in question")
+
     # Add question number to slide
     qmd_content += f"## Question {index + 1}\n\n"
 
@@ -33,13 +36,33 @@ def output_question(question, index):
         qmd_content += f"<video src='{video_file}' controls></video>\n\n"
 
     # Question itself (only displays on advance)
-    qmd_content += f"\n\n. . .\n\n{question['question']}\n\n"
+    if not revision_round:
+        qmd_content += f"\n\n. . ."
+    qmd_content += f"\n\n{question['question']}\n\n"
+
+    if not "choices" in question and revision_round:
+        qmd_content += f"\n\n. . .\n\n**{question['answer']}**\n\n"
+        
 
     # For multiple choice questions
     if "choices" in question:
+        # No incremental for the revision round
+        if revision_round:
+            qmd_content += "::: {.nonincremental}\n"
+
+        answer_index = question["choices"].index(question["answer"])
+
         for c_index, choice in enumerate(question["choices"]):
             letter = lingquiztics.tools.index_to_letter(c_index).upper()
-            qmd_content += f"{letter}.  {choice}\n"
+            correct_index = question["choices"].index(question["answer"])
+
+            if c_index == correct_index and mc_bold:
+                qmd_content += f"{letter}.  **<u>{choice}</u>**\n"
+            else:
+                qmd_content += f"{letter}.  {choice}\n"
+
+        if revision_round:
+            qmd_content += ":::\n"
 
         qmd_content += "\n\n"
 
@@ -47,5 +70,8 @@ def output_question(question, index):
     qmd_content += f"::: {{.notes}}\n\
 {lingquiztics.questions.make_text(question)}.\n\
 :::\n\n"
+    
+    if revision_round and "choices" in question and not mc_bold:
+        qmd_content += output_question(question, index, revision_round, mc_bold=True)
     
     return qmd_content
