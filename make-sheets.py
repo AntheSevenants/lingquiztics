25e0b9154424d503280aa05bae894037107fa0e9
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-10-11 00:52:13 +0200

    Allow specification of team names

--- make-sheets.py
+++ make-sheets.py
@@ -11,6 +11,7 @@ parser.add_argument('questions', type=str,
 					help='Path to the JSON file containing the questions')
 parser.add_argument('sheets_header', type=str,
 					help='Path to the Quarto file containing the answer sheets header')
+parser.add_argument('--team_names', type=str, nargs='?', default=None, help='Path to text file with team names (one per line)')
 args = parser.parse_args()
 
 with open(args.sheets_header, "rt") as reader:
@@ -20,33 +21,40 @@ qmd_content = f"{qmd_content}\n\n"
 
 rounds = lingquiztics.questions.load(args.questions)
 
-for round_no, quiz_round in enumerate(rounds):
-    print(quiz_round)
+team_names = [ " " ]
+if args.team_names is not None:
+    with open(args.team_names, "rt") as reader:
+        team_names = reader.read().split("\n")
 
-    questions = rounds[quiz_round]
+for team_name in team_names:
+    for round_no, quiz_round in enumerate(rounds):
+        print(quiz_round)
 
-    qmd_content += f"```{{=latex}}\n\
+        questions = rounds[quiz_round]
+
+        qmd_content += f"```{{=latex}}\n\
 \\begin{{tabularx}}{{\\textwidth}}{{c >{{\\raggedleft\\arraybackslash}}X}}\n\
-{quiz_round} & \\tiny De Vuurvretende Badeendjes\n\
+{quiz_round} & \\tiny {team_name}\n\
 \\end{{tabularx}}\n\
 ```\n\n"
 
-    qmd_content += "```{=latex}\n\
+        qmd_content += "```{=latex}\n\
 \\begingroup\n\
 \\setlength{\\tabcolsep}{20pt}\n\
 \\renewcommand{\\arraystretch}{1.5}\n\
 \\begin{tabularx}{\\textwidth}{|c|X|}\n\
 \hline\n"
 
-    for index, question in enumerate(questions):
-        qmd_content += f"{index + 1} & \\\\ \\hline\n"
+        for index, question in enumerate(questions):
+            qmd_content += f"{index + 1} & \\\\ \\hline\n"
 
-    qmd_content += "\\end{tabularx}\n\
+        qmd_content += "\\end{tabularx}\n\
 ```\n\n"
 
-    if round_no + 1 != len(rounds):
-        qmd_content += "\n\n{{< pagebreak >}}\n\n"
+        if round_no + 1 != len(rounds):
+            qmd_content += "\n\n{{< pagebreak >}}\n\n"
 
+    qmd_content += "\n\n{{< pagebreak >}}\n\n"
 
-with open("answer_sheets.qmd", "wt") as writer:
-    writer.write(qmd_content)
+    with open("answer_sheets.qmd", "wt") as writer:
+        writer.write(qmd_content)
