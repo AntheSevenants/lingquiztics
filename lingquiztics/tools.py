4e6014083eb0b8415ffd9a4ba918b455f7a05d24
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2023-10-11 04:13:44 +0200

    Move header generation to separate function

--- lingquiztics/tools.py
+++ lingquiztics/tools.py
@@ -2,3 +2,10 @@ def index_to_letter(index):
     alphabet = "abcdefghijklmnopqrstuvwxyz"
 
     return alphabet[index]
+
+def render_header(quiz_round, team_name):
+    return f"```{{=latex}}\n\
+\\begin{{tabularx}}{{\\textwidth}}{{c >{{\\raggedleft\\arraybackslash}}X}}\n\
+\Huge {quiz_round} & \\tiny {team_name}\n\
+\\end{{tabularx}}\n\
+```\n\n"
